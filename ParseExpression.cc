77a83e3de67eedb72e84d45d87b334b2bb03bb6b
gecko <none@none.none>
2022-01-10 21:46:22 +0200

    Work on expression parser

--- ParseExpression.cc
+++ ParseExpression.cc
@@ -11,7 +11,6 @@ bool Cap::SourceFile::parseExpression(size_t& i, Scope& current)
 
 	for(; i < current.end; i++)
 	{
-		DBG_LOG("expr %s '%s'", tokens[i].getTypeString(), tokens[i].getString().c_str());
 		size_t currentLine = tokens[i].line;
 
 		if(tokens[i].type == TokenType::Break)
@@ -38,6 +37,71 @@ bool Cap::SourceFile::parseExpression(size_t& i, Scope& current)
 
 			//	At this point we're definitely inside an expression
 			inExpression = true;
+
+			//	TODO add the identifier
+			DBG_LOG("Expression: Identifier '%s'", tokens[i].getString().c_str());
+		}
+
+		//	Check for numeric values
+		else if(tokens[i].type != TokenType::Operator)
+		{
+			//	TODO add the value
+			DBG_LOG("Expression: %s '%s'", tokens[i].getTypeString(), tokens[i].getString().c_str());
+		}
+
+		//	Check for operators
+		else
+		{
+			DBG_LOG("Expression: Operator '%s'", tokens[i].getString().c_str());
+			bool possiblyUnary = i == start || tokens[i - 1].type == TokenType::Operator;
+
+			//	Is the next token the same operator as this one
+			size_t next = i + 1;
+			if(isToken(TokenType::Operator, next) && *tokens[next].begin == *tokens[i].begin)
+			{
+				//	FIXME When not in parenthesis, check if the line changes
+				size_t gap = tokens.getIndex(next) - tokens.getIndex(i) - 1;
+				DBG_LOG("Gap is %lu", gap);
+
+				if(gap == 0)
+				{
+					DBG_LOG("Combine '%c'", *tokens[next].begin);
+
+					switch(*tokens[next].begin)
+					{
+						//	Make sure that there's an identifiers on the appropriate side
+						case '+': case '-':
+						{
+							break;
+						}
+
+						case '<': break;
+						case '>': break;
+						case '&': break;
+						case '|': break;
+						case '=': break;
+						case '*': break;
+
+						//	TODO make sure that there are identifiers on both sides
+						case '.': break;
+
+						default:
+							ERROR_LOG(tokens[i], "Invalid operator '%c%c'\n", *tokens[i].begin, *tokens[i].begin);
+							return true;
+					}
+
+					i = next;
+					continue;
+				}
+			}
+
+			else if(possiblyUnary)
+			{
+				DBG_LOG("Unary '%c'", *tokens[i].begin);
+				continue;
+			}
+
+			DBG_LOG("Normal operator '%c'", *tokens[i].begin);
 		}
 	}
 
